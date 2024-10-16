c18b1aafbb5b4fafc6c2004a0b786f8e5745f8c6
d48325758@alumnos.itr.edu.ar <d48325758@alumnos.itr.edu.ar>
2024-10-16 10:16:10 -0300

    Gamepage agregada

--- vite-project/src/Componentes/HomePage.jsx
+++ vite-project/src/Componentes/HomePage.jsx
@@ -4,7 +4,7 @@ import './HomePage.css';
 const HomePage = () => {
     return (
         <div className="home-page">
-            <h1>Juegos JOANTA</h1>
+            <h1>Juegos ANTAJO</h1>
             <p>Tu sitio de videojuegos preferido</p>
             <button>Prueba la beta</button>
         </div>
