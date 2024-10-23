cad78e549203f8a86020f7a491e4831b6bac4425
d48325758@alumnos.itr.edu.ar <d48325758@alumnos.itr.edu.ar>
2024-10-23 20:34:17 -0300

    finalizada la hompage

--- vite-project/src/Componentes/HomePage.jsx
+++ vite-project/src/Componentes/HomePage.jsx
@@ -5,8 +5,12 @@ const HomePage = () => {
     return (
         <div className="home-page">
             <h1 className='titulo'>Juegos ANTAJO</h1>
-            <p className='p'>Tu sitio de videojuegos preferido</p>
-            <button className='boton'>Prueba la beta</button>
+            <p className='p'>Tu sitio</p>
+            <p className='p2'>preferido</p>
+            <p className='p2'>de videojuegos</p>
+            <button className='boton'>
+                Prueba la beta
+            </button>
         </div>
     );
 };
--- vite-project/src/Componentes/Navbar.jsx
+++ vite-project/src/Componentes/Navbar.jsx
@@ -1,11 +1,12 @@
 import './Navbar.css';
 import React from 'react';
+import imagen from '../Imagenes/1.png';
 
 function Navbar() {
     return (
         <>
             <nav className="navbar">
-                <img id="logo" src="../Imagenes/1.png" ></img>
+                <img src={imagen} ></img>
                 <ul>
                     <li><a href="/HomePage.jsx">Inicio</a></li>
                     <li><a href="#">Juego</a></li>
