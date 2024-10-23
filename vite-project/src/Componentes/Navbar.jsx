83ed19a74d0f514dfe9fa3a4c6c91342b87a3bec
d48325758@alumnos.itr.edu.ar <d48325758@alumnos.itr.edu.ar>
2024-10-23 09:48:31 -0300

    creo que ya esta la navbar

--- vite-project/src/Componentes/Navbar.jsx
+++ vite-project/src/Componentes/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
     return (
         <>
             <nav className="navbar">
-                <img id="logo" src="INCLUIR_RUTA_DE_IMAGEN" ></img>
+                <img id="logo" src="../Imagenes/1.png" ></img>
                 <ul>
                     <li><a href="/HomePage.jsx">Inicio</a></li>
                     <li><a href="#">Juego</a></li>
